ecb0d2aa7cd15645550aa87d88e6cd1ccd89c917
Nithin Gangadharan <nithin.linkin@gmail.com>
2020-09-28 00:49:56 -0400

    Fix the message segments tests

--- tests/test_message_segmenting.py
+++ tests/test_message_segmenting.py
@@ -52,22 +52,22 @@ class TestMessageSegmenting:
     def test_message_profiling_ucs2(self, short_ucs2_text, long_ucs2_text):
         profiled_message = profiling.profile_message(short_ucs2_text)
         assert profiled_message["num_segments"] == 1
-        assert profiled_message["message_length"] == 14
+        assert profiled_message["message_length"] == 15
         assert len(profiled_message["segments"]) == 1
         assert profiled_message["max_segment_size"] == (constants.UCS2_MAX_SEGMENT_SIZE // 2)
         self.assert_segment_lengths_for_profiled_message(
             profiled_message,
-            expected_total_segment_lengths=[14]
+            expected_total_segment_lengths=[15]
         )
 
         profiled_message = profiling.profile_message(long_ucs2_text)
-        assert profiled_message["num_segments"] == 5
-        assert profiled_message["message_length"] == 334
-        assert len(profiled_message["segments"]) == 5
+        assert profiled_message["num_segments"] == 6
+        assert profiled_message["message_length"] == 336
+        assert len(profiled_message["segments"]) == 6
         assert profiled_message["max_segment_size"] == (constants.UCS2_MAX_CONCAT_SEGMENT_SIZE // 2)
         self.assert_segment_lengths_for_profiled_message(
             profiled_message,
-            expected_total_segment_lengths=[67, 67, 67, 67, 66]
+            expected_total_segment_lengths=[67, 67, 67, 67, 67, 1]
         )
 
     def test_message_profiling_mms_gsm7(self, short_gsm7_text, long_gsm7_text, byte_string_gsm7_chars,
@@ -115,20 +115,20 @@ class TestMessageSegmenting:
     def test_message_profiling_mms_ucs2(self, short_ucs2_text, long_ucs2_text):
         profiled_message = profiling.profile_message(short_ucs2_text, is_for_mms=True)
         assert profiled_message["num_segments"] == 1
-        assert profiled_message["message_length"] == 14
+        assert profiled_message["message_length"] == 15
         assert len(profiled_message["segments"]) == 1
         assert profiled_message["max_segment_size"] == constants.MMS_MAX_SEGMENT_SIZE
         self.assert_segment_lengths_for_profiled_message(
             profiled_message,
-            expected_total_segment_lengths=[14]
+            expected_total_segment_lengths=[15]
         )
 
         profiled_message = profiling.profile_message(long_ucs2_text, is_for_mms=True)
         assert profiled_message["num_segments"] == 1
-        assert profiled_message["message_length"] == 334
+        assert profiled_message["message_length"] == 336
         assert len(profiled_message["segments"]) == 1
         assert profiled_message["max_segment_size"] == constants.MMS_MAX_SEGMENT_SIZE
         self.assert_segment_lengths_for_profiled_message(
             profiled_message,
-            expected_total_segment_lengths=[334]
+            expected_total_segment_lengths=[336]
         )
